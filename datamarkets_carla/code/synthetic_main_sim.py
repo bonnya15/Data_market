b697192bbecf7e0f38d9bf690f9182c5b2a4a88d
bonnya15 <ghoshshiulisubhra16@gmail.com>
2021-10-24 19:57:52 +0530

    adding git setup files

--- datamarkets_carla/code/synthetic_main_sim.py
+++ datamarkets_carla/code/synthetic_main_sim.py
@@ -9,7 +9,7 @@
 import numpy as np
 from sklearn.linear_model import LinearRegression
 import pandas as pd
-import multiprocessing
+#import multiprocessing
 
 # ---------------------------------------------------------------------------#
 # ----------------------------- SCRIPT CONTENTS -----------------------------#
