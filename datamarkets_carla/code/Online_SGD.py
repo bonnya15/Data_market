de31b6e23ce6aeb5da364676ba91928aa30ed377
sumanroyal <sumanroy2851995@gmail.com>
2021-10-28 15:02:13 +0530

    1

--- datamarkets_carla/code/Online_SGD.py
+++ datamarkets_carla/code/Online_SGD.py
@@ -8,47 +8,64 @@ import pandas as pd
 
 
 class Online_SGD:
-    def __init__(self, X, y, learning_rate=0.2):
-        self.X = X
-        self.y = y
+
+    def __init__(self, learning_rate=0.2, n_epochs=100, k=40, damp_factor=1.02):
         self.learning_rate = learning_rate
-        self.w = np.zeros((1, self.X.shape[1]))  # Randomly initializing weights
+        self.n_epochs = n_epochs
+        self.k = k
+        self.damp_factor = damp_factor
+
+    def fit(self, X, Y):
+        self.w = np.zeros((1, X.shape[1]))  # Randomly initializing weights
         self.b = np.zeros((1, 1))
-
-    def fit(self):
-        for i in range(self.X.shape[0]):
-            Lw = (self.X[i]) * (self.y[i] - np.dot(self.X[i], self.w.T) - self.b)
-            Lb = (self.y[i] - np.dot(self.X[i], self.w.T) - self.b)
-            self.w = self.w - self.learning_rate * Lw
-            self.b = self.b - self.learning_rate * Lb
-            self.learning_rate = self.learning_rate / 1.02
-
+        #         print("Shape of W",self.w.shape)
+        #         print("Shape of b",self.b.shape)
+        #         print("W",self.w)
+        #         print("b",self.b)
+        for i in range(X.shape[0]):
+            #             print("shape of X[i]",X[i].shape)
+            #             print("shape of Y[i]",Y[i].shape)
+            #             print("shape of w.T",self.w.T.shape)
+            x = (np.array(X))[i].reshape(1, X.shape[1])
+            y = (np.array(Y))[i].reshape(1, 1)
+            #             print("shape of x",x.shape)
+            #             print("shape of y]",y.shape)
+            Lw = np.dot((y - np.dot(x, self.w.T) - self.b), x)
+            Lb = (y - np.dot(x, self.w.T) - self.b)
+            self.w = self.w + self.learning_rate * Lw
+            self.b = self.b + self.learning_rate * Lb
+            #             print("W",self.w)
+            #             print("b",self.b)
+            self.learning_rate = self.learning_rate / self.damp_factor
         return self.w, self.b
 
-    def transform(self):
-        return np.dot(self.X, self.w.T) + self.b
+    def fit_batch(self, X, Y):
+        self.w = np.zeros((1, X.shape[1]))  # Randomly initializing weights
+        self.b = np.zeros((1, 1))
+        n = 1
+        while n <= self.n_epochs:
+            temp = pd.merge(pd.DataFrame(X), pd.DataFrame(Y), how='left')
+            temp2 = temp.sample(self.k)
+            X_tr = temp2.iloc[:, 0:temp.shape[1]].values
+            Y_tr = temp2.iloc[:, -1].values
 
-    def error(self):
-        y_pred = np.dot(self.X, self.w.T) + self.b
-        loss = mean_squared_error(y_pred, self.y)
-        print(loss)
+            for i in range(self.k):
+                x = X_tr[i].reshape(1, X_tr.shape[1])
+                y = Y_tr[i].reshape(1, 1)
+                #                 print("shape of x",x.shape)
+                #                 print("shape of y",y.shape)
+                Lw = (np.dot((y - np.dot(x, self.w.T) - self.b), x)) * (2 / self.k)
+                Lb = ((y - np.dot(x, self.w.T) - self.b)) * (2 / self.k)
+                self.w = self.w + self.learning_rate * Lw
+                self.b = self.b + self.learning_rate * Lb
+            #             print("W",self.w)
+            #             print("b",self.b)
 
-    def plot(self):
-        from matplotlib.pyplot import figure
-        y_pred = np.dot(self.X, self.w.T) + self.b
-        plt.figure(figsize=(25, 6))
-        plt.plot(self.y, label='Actual')
-        plt.plot(y_pred, label='Predicted')
-        plt.legend(prop={'size': 16})
-        plt.show()
-        print('Mean Squared Error :', mean_squared_error(y_pred, self.y))
+            self.learning_rate = self.learning_rate / 1
+            n = n + 1
+        return self.w, self.b
 
-
-
-# data = pd.read_csv("dataset.csv")
-# y = data["Profit"]
-# X = np.array(data["Population"])
-# X1 = X.reshape(X.shape[0], -1)
-# lr = Online_SGD(X1, y)
-# lr.fit()
-# lr.transform()
+    def predict(self, X):
+        m = np.dot(X, self.w.T) + self.b
+        n = m.reshape(-1, )
+        return n
