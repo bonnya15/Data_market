43011cd55bee3055fd1a53a330b7824de17aaaba
sumanroyal <sumanroy2851995@gmail.com>
2021-11-02 11:27:43 +0530

    updated online and normal

--- datamarkets_carla/code/Online_SGD.py
+++ datamarkets_carla/code/Online_SGD.py
@@ -1,71 +1,98 @@
-import warnings
-
-warnings.filterwarnings("ignore")
-import numpy as np
-import matplotlib.pyplot as plt
-from sklearn.metrics import mean_squared_error
-import pandas as pd
-
-
 class Online_SGD:
+    import warnings
+    warnings.filterwarnings("ignore")
+    import numpy as np
+    import pandas as pd
 
-    def __init__(self, learning_rate=0.2, n_epochs=100, k=40, damp_factor=1.02):
+    def __init__(self, learning_rate=0.2, n_epochs=100, k=40, damp_factor=1.02, tol=1e-2, w_file="w.txt",
+                 b_file="b.txt"):
         self.learning_rate = learning_rate
         self.n_epochs = n_epochs
         self.k = k
         self.damp_factor = damp_factor
+        self.tol = tol
+        self.w_file = w_file
+        self.b_file = b_file
 
-    def fit(self, X, Y):
-        self.w = np.zeros((1, X.shape[1]))  # Randomly initializing weights
-        self.b = np.zeros((1, 1))
-        #         print("Shape of W",self.w.shape)
-        #         print("Shape of b",self.b.shape)
-        #         print("W",self.w)
-        #         print("b",self.b)
+    def fit_online(self, X, Y):
+
+        self.w = np.loadtxt(self.w_file).reshape(1, X.shape[1])  # Reading weights and bias
+        self.b = np.loadtxt(self.b_file).reshape(1, 1)
+
         for i in range(X.shape[0]):
-            #             print("shape of X[i]",X[i].shape)
-            #             print("shape of Y[i]",Y[i].shape)
-            #             print("shape of w.T",self.w.T.shape)
             x = (np.array(X))[i].reshape(1, X.shape[1])
             y = (np.array(Y))[i].reshape(1, 1)
-            #             print("shape of x",x.shape)
-            #             print("shape of y]",y.shape)
             Lw = np.dot((y - np.dot(x, self.w.T) - self.b), x)
             Lb = (y - np.dot(x, self.w.T) - self.b)
             self.w = self.w + self.learning_rate * Lw
             self.b = self.b + self.learning_rate * Lb
-            #             print("W",self.w)
-            #             print("b",self.b)
             self.learning_rate = self.learning_rate / self.damp_factor
+
+        a_file = open(self.w_file, "w")
+        for row in self.w:
+            np.savetxt(a_file, row)
+        a_file.close()
+        b_file = open(self.b_file, "w")
+        for row in self.b:
+            np.savetxt(b_file, row)
+        b_file.close()
+
         return self.w, self.b
 
-    def fit_batch(self, X, Y):
-        self.w = np.zeros((1, X.shape[1]))  # Randomly initializing weights
-        self.b = np.zeros((1, 1))
-        n = 1
-        while n <= self.n_epochs:
-            temp = pd.merge(pd.DataFrame(X), pd.DataFrame(Y), how='left')
-            temp2 = temp.sample(self.k)
-            X_tr = temp2.iloc[:, 0:temp.shape[1]].values
-            Y_tr = temp2.iloc[:, -1].values
+    def fit_regression(self, X, Y):
+        from sklearn.linear_model import LinearRegression
+        reg = LinearRegression()
+        reg.fit(X, Y)
+        self.w = reg.coef_.reshape(1, X.shape[1])
+        self.b = reg.intercept_.reshape(1, 1)
+        a_file = open(self.w_file, "w")
+        for row in self.w:
+            np.savetxt(a_file, row)
+        a_file.close()
+        b_file = open(self.b_file, "w")
+        for row in self.b:
+            np.savetxt(b_file, row)
+        b_file.close()
 
-            for i in range(self.k):
-                x = X_tr[i].reshape(1, X_tr.shape[1])
-                y = Y_tr[i].reshape(1, 1)
-                #                 print("shape of x",x.shape)
-                #                 print("shape of y",y.shape)
-                Lw = (np.dot((y - np.dot(x, self.w.T) - self.b), x)) * (2 / self.k)
-                Lb = ((y - np.dot(x, self.w.T) - self.b)) * (2 / self.k)
-                self.w = self.w + self.learning_rate * Lw
-                self.b = self.b + self.learning_rate * Lb
-            #             print("W",self.w)
-            #             print("b",self.b)
-
-            self.learning_rate = self.learning_rate / 1
-            n = n + 1
         return self.w, self.b
 
     def predict(self, X):
+        self.w = np.loadtxt(self.w_file).reshape(1, X.shape[1])  # Reading weights and bias
+        self.b = np.loadtxt(self.b_file).reshape(1, 1)
         m = np.dot(X, self.w.T) + self.b
         n = m.reshape(-1, )
+
         return n
+
+#     def fit_batch(self,X,Y):
+#         self.w = np.zeros((1, X.shape[1]))  # Randomly initializing weights
+#         self.b = np.zeros((1, 1))
+#         n=1
+#         while n<=self.n_epochs:
+#             temp = pd.concat([pd.DataFrame(X), pd.DataFrame(Y)], axis=1)
+#             temp2=temp.sample(self.k)
+#             X_tr = temp2.iloc[:,0:-1]
+#             Y_tr = temp2.iloc[:,-1]
+# #             print("shape of x_tr",X_tr.shape)
+# #             print("temp shape",temp.shape,temp2.shape)
+#             Lw=np.zeros((1, X_tr.shape[1]))
+#             Lb=np.zeros((1, 1))
+
+#             for i in range(self.k):
+#                 x=(np.array(X_tr))[i].reshape(1,X_tr.shape[1])
+#                 y=(np.array(Y_tr))[i].reshape(1,1)
+# #                 print("shape of x",x.shape)
+# #                 print("shape of y",y.shape)
+#                 Lw = Lw+(np.dot((y - np.dot(x, self.w.T) - self.b),x))
+#                 Lb = Lb+((y - np.dot(x, self.w.T) - self.b))
+#             self.w = self.w + self.learning_rate * Lw
+#             self.b = self.b + self.learning_rate * Lb
+#         #             print("W",self.w)
+#         #             print("b",self.b)
+
+
+#             self.learning_rate = self.learning_rate / self.damp_factor
+#             n=n+1
+#         return self.w,self.b
+
+
